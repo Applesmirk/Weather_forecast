3e433d7881c5425d7abafb189837124e924c7ba1
christian <christian.amesoeder@stud.uni-regensburg.de>
2020-06-19 22:00:19 +0200

    updated main for the first 3 submodules

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
-import datetime
-import dwdweather
+from get_data import *
+from read_data import read_data
+import numpy as np 
+from train_prev_days import * 
 
+station_id = 4104
+download_id(station_id)
+data = read_data(station_id)
 
-print("Hello")
+prepare_data_days(data)
