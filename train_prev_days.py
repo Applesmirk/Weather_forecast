30ad92e3f0abae1aa149dd2e42d7add4eea46d3f
jola29 <joshualappat@aol.com>
2020-06-20 14:49:28 +0200

    implemented split_data function

--- train_prev_days.py
+++ train_prev_days.py
@@ -1,15 +1,36 @@
 import numpy as np
 
-def prepare_data_days(data, range_= 10, month=1, days=3): 
+def prepare_data_days(data, range_= 1, days=3): 
 
     data = data.astype(float)
     data = data[-(365*range_):,:]
     
-    xtrain, ytrain = [], []
+    xdata, ydata = [], []
     for i in range(data.shape[0]): 
-        if i + days >= len(data.shape[0]): break 
+        if i + days >= data.shape[0]: break 
         
-        xtrain.append(data[i:i + days,:])
-        ytrain.append(data[i + days,:])
+        xdata.append(data[i:i + days,:])
+        ydata.append(data[i + days,:])
+
+    return xdata, ydata
+
+
+def split_data(xdata, ydata, faktor = .1, count = 0):
+    '''
+    expects:
+        xdata,ydata lists
+        factor: percentage of test data
+        count: integer, data splitted into parts size *factor, select the counter-th part
+    '''
+    #compute the startin/end point
+    start = int(faktor * count * len(xdata))
+    end = int(faktor * (count + 1) * len(xdata))
     
-    print(ytrain[-1])
+    #create training and test sets
+    xtest, ytest= xdata[start:end], ydata[start:end]
+    del xdata[start:end]
+    del ydata[start:end]
+
+    return (xdata,ydata), (xtest,ytest)
+    
+
