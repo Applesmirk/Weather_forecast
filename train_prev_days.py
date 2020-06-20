80d40b8a9e448d32d32917d485c33f217f2db4ab
jola29 <joshualappat@aol.com>
2020-06-20 14:16:08 +0200

    changed for loop a little bit

--- train_prev_days.py
+++ train_prev_days.py
@@ -7,7 +7,7 @@ def prepare_data_days(data, range_= 10, month=1, days=3):
     
     xtrain, ytrain = [], []
     for i in range(data.shape[0]): 
-        if i + days >= len(data[:,0]): break 
+        if i + days >= len(data.shape[0]): break 
         
         xtrain.append(data[i:i + days,:])
         ytrain.append(data[i + days,:])
