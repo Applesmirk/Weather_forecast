c9452fb56a63f2cb523bd9a3d37183b2ec9175a6
jola29 <joshualappat@aol.com>
2020-06-20 14:48:44 +0200

    implemented train_prev_days

--- Main.py
+++ Main.py
@@ -7,4 +7,7 @@ station_id = 4104
 download_id(station_id)
 data = read_data(station_id)
 
-prepare_data_days(data)
+xdata, ydata = prepare_data_days(data)
+
+(xtrain,ytrain), (xtest,ytest) = split_data(xdata, ydata, count = 5)
+print(len(xtrain))
